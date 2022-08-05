678f1b0762415c1c07dedbc82f69cb4f2cffcd51
HeroicHitesh <email.hiteshkumar@gmail.com>
2022-08-05 14:14:52 +0530

    ✨ (app): Create HomePage Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
