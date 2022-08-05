0458bcc7398b3f7839c74514835f4d507997392c
HeroicHitesh <email.hiteshkumar@gmail.com>
2022-08-05 15:09:18 +0530

    ✨ (ArticlePage): Add article-content

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import articleContent from './article-content';
 
 const ArticlePage = () => {
     const { name } = useParams();
+    const article = articleContent.find(article => article.name === name);
 
     return (
         <>
-            <h1>This is the {name} article</h1>
+            <h1>{article.title}</h1>
+            {article.content.map((paragraph, key) => (
+                <p key={key}>{paragraph}</p>
+            ))}
         </>
     );
 }
