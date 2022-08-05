943c9db88f2ae2f7109db020623d3b9de4fd2e6e
HeroicHitesh <email.hiteshkumar@gmail.com>
2022-08-05 13:58:55 +0530

    ✨ (HomePage): Create HomePage component

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+        <h1>Hello, welcome to my blog!</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi eum ea nihil fuga magnam fugit quae reprehenderit natus, temporibus sed possimus cum nam exercitationem veniam dicta molestiae modi corrupti. In?</p>
+        <p>Laborum, autem necessitatibus quod est laboriosam ratione nam temporibus quasi ducimus odio consectetur harum. Rem saepe, explicabo illum perspiciatis, sunt ullam quas at fugit eum nihil necessitatibus possimus maxime. Vitae.</p>
+        <p>Repellat ut excepturi corrupti et cupiditate ipsam, tenetur quia voluptates minima ipsa, numquam iste totam! Magnam, sapiente assumenda autem soluta enim eos quo qui voluptatum ea deleniti fugiat ipsum. Qui.</p>
+    </>
+);
+
+export default HomePage;
