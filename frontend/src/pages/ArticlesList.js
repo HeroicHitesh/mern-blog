f7f515c7c4fe6509619125f11e8266944392a3bc
HeroicHitesh <email.hiteshkumar@gmail.com>
2022-08-05 15:17:18 +0530

    ✨ (ArticlesList): List and link all articles

--- frontend/src/pages/ArticlesList.js
+++ frontend/src/pages/ArticlesList.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import articleContent from './article-content';
 
 const ArticlesList = () => (
     <>
         <h1>Articles</h1>
+        {articleContent.map((article, key) => (
+            <Link key={key} to={`/article/${article.name}`}>
+                <h3>{article.title}</h3>
+            </Link>
+        ))}
     </>
 );
 
