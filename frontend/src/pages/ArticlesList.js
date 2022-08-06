77ff6d1156f74037fd15c9e3873499565979ce0c
HeroicHitesh <email.hiteshkumar@gmail.com>
2022-08-06 12:48:41 +0530

    ✨ (ArticlesList): Show article samples

--- frontend/src/pages/ArticlesList.js
+++ frontend/src/pages/ArticlesList.js
@@ -8,6 +8,7 @@ const ArticlesList = () => (
         {articleContent.map((article, key) => (
             <Link className='article-list-item' key={key} to={`/article/${article.name}`}>
                 <h3>{article.title}</h3>
+                <p>{article.content[0].substring(0, 150)}...</p>
             </Link>
         ))}
     </>
