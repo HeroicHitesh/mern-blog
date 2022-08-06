47c3f49647caf50cf253b494084d0385fc86a9ca
HeroicHitesh <email.hiteshkumar@gmail.com>
2022-08-06 17:30:40 +0530

    💩 (ArticlePage): POC, Add random upvotes using hooks

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from './NotFoundPage';
@@ -8,12 +8,19 @@ const ArticlePage = () => {
     const { name } = useParams();
     const article = articleContent.find(article => article.name === name);
 
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+    useEffect(() => {
+        setArticleInfo({ upvotes: Math.ceil(Math.random() * 10) });
+    }, [name]);
+
     const otherArticles = articleContent.filter(article => article.name !== name);
 
     if (!article) return <NotFoundPage />
     return (
         <>
             <h1>{article.title}</h1>
+            <p>This post has been upvoted {articleInfo.upvotes} times.</p>
             {article.content.map((paragraph, key) => (
                 <p key={key}>{paragraph}</p>
             ))}
