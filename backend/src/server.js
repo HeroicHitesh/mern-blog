ea11208ca711b7566d08c128d968b267b9f5b9e6
HeroicHitesh <email.hiteshkumar@gmail.com>
2022-08-06 14:42:38 +0530

    💩 (DB): Add temp JSON database (DB)

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+const articlesInfo = {
+    'learn-react': {
+        upvotes: 0,
+    },
+    'learn-node': {
+        upvotes: 0,
+    },
+    'my-thoughts-on-resumes': {
+        upvotes: 0,
+    },
+}
+
 const app = express();
 
 app.use(bodyParser.json());
