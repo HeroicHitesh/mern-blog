064850c42f30fa0e8f7188c0b23763399cc76105
HeroicHitesh <email.hiteshkumar@gmail.com>
2022-08-06 17:53:43 +0530

    ✨ (CommentsList): Show comments in ArticlePage

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import ArticlesList from '../components/ArticlesList';
+import CommentsList from '../components/CommentsList';
 import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content';
 
@@ -29,6 +30,7 @@ const ArticlePage = () => {
             {article.content.map((paragraph, key) => (
                 <p key={key}>{paragraph}</p>
             ))}
+            <CommentsList comments={articleInfo.comments} />
             <h3>Other Articles:</h3>
             <ArticlesList articles={otherArticles} />
         </>
