673860e8bad92a7e95b22d804322ca22b0932b6e
HeroicHitesh <email.hiteshkumar@gmail.com>
2022-08-06 17:12:28 +0530

    🐛 (server): Fix add-comment endpoint using withDB()

--- backend/src/server.js
+++ backend/src/server.js
@@ -48,8 +48,17 @@ app.post('/api/articles/:name/add-comment', (req, res) => {
     const { username, text } = req.body;
     const articleName = req.params.name;
 
-    articlesInfo[articleName].comments.push({ username, text });
-    res.status(200).send(articlesInfo[articleName]);
+    withDB(async (db) => {
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        await db.collection('articles').updateOne({ name: articleName }, {
+            '$set': {
+                comments: articleInfo.comments.concat({ username, text }),
+            },
+        });
+
+        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+        res.status(200).json(updatedArticleInfo);
+    }, res);
 });
 
 app.listen(8000, () => console.log('Listening on port 8000'));
