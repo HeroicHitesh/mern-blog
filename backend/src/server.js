361f5da4210944a05b65e09cd57b96507b1563fd
HeroicHitesh <email.hiteshkumar@gmail.com>
2022-08-06 14:44:20 +0530

    ✨ (server): Add /articles/:name/upvote endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,4 +17,11 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const articleName = req.params.name;
+
+    articlesInfo[articleName].upvotes += 1;
+    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`);
+});
+
 app.listen(8000, () => console.log('Listening on port 8000'));
